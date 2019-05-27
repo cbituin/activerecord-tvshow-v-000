0746ca71176993b78dd28067ad338de1135a5ce8
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-05-27 06:01:41 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-    self.where(self.highest_rating, [:rating])
+    self.where(self.highest_rating, self[:rating])
   end
 
 end
