44a362b7a27d5983fdbd4578ead80b2941706c54
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-05-27 06:04:00 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,4 +12,18 @@ class Show < ActiveRecord::Base
     self.minimum(:rating)
   end
 
+  def self.least_popular_show
+  end
+
+  def self.ratings_sum
+
+  end
+
+  def self.popular_shows
+  end
+
+  def self.shows_by_alphabetical_order
+
+  end
+
 end
